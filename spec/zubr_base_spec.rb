e3a4b96abf05ba10029c242abce951404dd9a01c
Sergey Doryba <Golfersds@gmail.com>
2015-01-13 14:35:46 +0200

    fixed test zubr base request to home page

--- spec/zubr_base_spec.rb
+++ spec/zubr_base_spec.rb
@@ -1,37 +1,22 @@
 describe Zubr do
-  include Rack::Test::Methods
   describe 'get response status 200' do
     it 'has response status success' do
       get '/'
-      #expect(last_response).to be_ok
-      p last_response
-      p last_response.body
-      p last_response.status
+      expect(last_response).to be_ok
+      expect(last_response.status).to eq 200
     end
 
-    it 'has response status success' do
+    xit 'has response status success' do
       get '/not_be_qood_status_response'
-      #expect(last_response).to be_ok
-      p last_response
-      p last_response.body
-      p last_response.status
     end
 
-    it 'has response status taste' do
+    xit 'has response status taste' do
       get '/taste'
-      #expect(last_response).to be_ok
-      p last_response
-      p last_response.body
-      p last_response.status
     end
 
 
-    it 'has response status cookorama' do
+    xit 'has response status cookorama' do
       get '/cookorama'
-      #expect(last_response).to be_ok
-      p last_response
-      p last_response.body
-      p last_response.status
     end
   end
 end
